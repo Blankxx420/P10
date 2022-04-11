588cd9c369ceb384461b43a1bdf384f2a739a2c7
blankxx <Shinima16>
2022-04-11 18:06:22 +0200

    correction fichier .travis.yml

--- djangoProject/settings/travis.py
+++ djangoProject/settings/travis.py
@@ -5,10 +5,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blankxx',
-        'USER': 'blankxx',
-        'PASSWORD': 'enurox123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     },
 }
