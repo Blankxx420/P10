3dc95cd06ebd90d6e7ccaad6bb1ee8a14eef37e1
blankxx <Shinima16>
2022-04-12 20:24:02 +0200

    correction fichier .travis.yml

--- users/test/functional_tests/test_authentification.py
+++ users/test/functional_tests/test_authentification.py
@@ -8,7 +8,7 @@ class SeleniumRegisterTest(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.selenium = webdriver.Chrome("/home/travis/virtualenv/python3.9.5/lib/site-packages/chromedriver")
+        cls.selenium = webdriver.Chrome("/home/travis/virtualenv/python3.9.5/bin")
         cls.selenium.implicitly_wait(10)
 
     @classmethod
