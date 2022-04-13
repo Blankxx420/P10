a63b8dbb07fea843b9d5764c8f015007860fba3d
blankxx <Shinima16>
2022-04-13 08:59:55 +0200

    correction fichier .travis.yml

--- users/test/functional_tests/test_authentification.py
+++ users/test/functional_tests/test_authentification.py
@@ -33,17 +33,15 @@ class SeleniumRegisterTest(StaticLiveServerTestCase):
         """Launches the functional_tests for the registration and automatical loggin feature"""
         # Access register page and fill fields
         self.selenium.get("%s%s" % (self.live_server_url, "/register/"))
-        email_input = self.selenium.find_element_by_name("email")
+        email_input = self.selenium.find_element(By.NAME, "email")
         email_input.send_keys("usertest1@email.com")
-        username_input = self.selenium.find_element_by_name("username")
+        username_input = self.selenium.find_element(By.NAME,"username")
         username_input.send_keys("usertest1")
-        password_input = self.selenium.find_element_by_name("password1")
+        password_input = self.selenium.find_element(By.NAME,"password1")
         password_input.send_keys("Password+1234")
-        confirm_password_input = self.selenium.find_element_by_name(
-            "password2"
-        )
+        confirm_password_input = self.selenium.find_element(By.NAME, "password2")
         confirm_password_input.send_keys("Password+1234")
         # Click on button which registers + login automatically
         self.selenium.find_element(By.XPATH, "//*[@id='register']/div/form/button").click()
         # Checks if icon "mon_compte" in DOM, means logged in
-        self.selenium.find_element_by_id("mon_compte")
+        self.selenium.find_element(By.ID, "mon_compte")
