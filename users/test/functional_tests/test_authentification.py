53ce0e7e94e8b96cb8034ee53ecb41e1bb163076
blankxx <Shinima16>
2022-04-13 18:32:30 +0200

    correction fichier .travis.yml

--- users/test/functional_tests/test_authentification.py
+++ users/test/functional_tests/test_authentification.py
@@ -40,6 +40,7 @@ class SeleniumRegisterTest(StaticLiveServerTestCase):
         confirm_password_input.send_keys("Password+1234")
         # Click on button which registers + login automatically
         submit_btn = self.driver.find_elements_by_xpath("//*[@id='register']/div/form/button")
-        submit_btn[0].click()
+        submit_btn.location_once_scrolled_into_view
+        submit_btn.click()
         # Checks if icon "mon_compte" in DOM, means logged in
         self.driver.find_element(By.ID, "mon_compte")
