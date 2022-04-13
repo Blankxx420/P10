fe2421eb02872f9ca9703ec778bfacd4b29d0022
blankxx <Shinima16>
2022-04-13 18:22:25 +0200

    correction fichier .travis.yml

--- users/test/functional_tests/test_authentification.py
+++ users/test/functional_tests/test_authentification.py
@@ -14,32 +14,32 @@ class SeleniumRegisterTest(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.selenium = webdriver.Firefox(
+        cls.driver = webdriver.Firefox(
             executable_path=str(BASE_DIR / 'webdrivers' / 'geckodriver'),
             options=firefox_options,
         )
-        cls.selenium.implicitly_wait(10)
-        cls.selenium.maximize_window()
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
 
     @classmethod
     def tearDownClass(cls):
-        cls.selenium.quit()
+        cls.driver.quit()
         super().tearDownClass()
 
     def test_register(self):
         """Launches the functional_tests for the registration and automatical loggin feature"""
         # Access register page and fill fields
-        self.selenium.get("%s%s" % (self.live_server_url, "/register/"))
-        email_input = self.selenium.find_element(By.NAME, "email")
+        self.driver.get("%s%s" % (self.live_server_url, "/register/"))
+        email_input = self.driver.find_element(By.NAME, "email")
         email_input.send_keys("usertest1@email.com")
-        username_input = self.selenium.find_element(By.NAME,"username")
+        username_input = self.driver.find_element(By.NAME,"username")
         username_input.send_keys("usertest1")
-        password_input = self.selenium.find_element(By.NAME,"password1")
+        password_input = self.driver.find_element(By.NAME,"password1")
         password_input.send_keys("Password+1234")
-        confirm_password_input = self.selenium.find_element(By.NAME, "password2")
+        confirm_password_input = self.driver.find_element(By.NAME, "password2")
         confirm_password_input.send_keys("Password+1234")
         # Click on button which registers + login automatically
-        submit_btn = self.selenium.find_elements_by_xpath("//*[@id='register']/div/form/button")
+        submit_btn = self.driver.find_elements_by_xpath("//*[@id='register']/div/form/button")
         submit_btn[0].click()
         # Checks if icon "mon_compte" in DOM, means logged in
-        self.selenium.find_element(By.ID, "mon_compte")
+        self.driver.find_element(By.ID, "mon_compte")
