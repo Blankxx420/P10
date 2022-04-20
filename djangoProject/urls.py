b3ed3a316710cc96ff7b063dd4c4aa5fef57261a
blankxx <Shinima16>
2022-04-20 21:19:17 +0200

    mise en place sentry

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -22,9 +22,6 @@ from users import views as users_views
 
 def trigger_error(request):
     division_by_zero = 1 / 0
-    return division_by_zero, request
-
-
 
 urlpatterns = [
     path('sentry-debug/', trigger_error),
